667c2a2d31c84f2565c61be316b8e5d82d36a1d0
Lepikhin Egor <lepikhin.egor@yandex.ru>
2020-05-19 11:43:12 +0300

    add scripts which check request paid

---
+++ modules/core/src/com/egorl/thesiscars/scripts/PaidCheckScript.groovy
@@ -0,0 +1,7 @@
+package com.egorl.thesiscars.scripts
+
+import com.egorl.thesiscars.entity.CarPurchaseRequest
+
+CarPurchaseRequest purchaseRequest = card
+boolean paid = purchaseRequest.getPaid()
+return paid
