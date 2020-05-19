40416126cfd44276ec2bc47b69f15fd87e3c3691
Lepikhin Egor <lepikhin.egor@yandex.ru>
2020-05-19 14:31:50 +0300

    regenerate db init scripts

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -30,30 +30,6 @@ create table CARS_CAR (
     primary key (CARD_ID)
 )^
 -- end CARS_CAR
---Add default numerator for cars$Car
-CREATE OR REPLACE FUNCTION baseInsert()
-RETURNS integer
-AS $$
-DECLARE
-    cnt integer = 0;
-BEGIN
-cnt = (select count(id) from DF_NUMERATOR where CODE = 'CarNumerator' and delete_ts is null);
-if(cnt = 0) then
-    INSERT INTO DF_NUMERATOR (ID, CREATE_TS, CREATED_BY, VERSION, CODE, NUMERATOR_FORMAT, SCRIPT_ENABLED,
-    PERIODICITY, NUMBER_INITIAL_VALUE, LOC_NAME)
-    VALUES ('867f8465-e37f-4743-a9eb-ef3d2856fcdb', now(), 'system', 1, 'CarNumerator', 'CAR-[number]', FALSE, 'Y', 1,
-    '{"captionWithLanguageList":[{"language":"ru","caption":"Car"},{"language":"en","caption":"Car"}]}'
-    );
-end if;
-
-return 0;
-END;
-$$
-LANGUAGE plpgsql;
-^
-
-select baseInsert()^
-drop function if exists baseInsert()^
 -- begin CARS_CAR_PURCHASE_REQUEST
 create table CARS_CAR_PURCHASE_REQUEST (
     CARD_ID uuid,
@@ -66,7 +42,30 @@ create table CARS_CAR_PURCHASE_REQUEST (
     primary key (CARD_ID)
 )^
 -- end CARS_CAR_PURCHASE_REQUEST
+--Add default numerator for cars$Car
+CREATE OR REPLACE FUNCTION baseInsert()
+RETURNS integer
+AS $$
+DECLARE
+    cnt integer = 0;
+BEGIN
+cnt = (select count(id) from DF_NUMERATOR where CODE = 'CarNumerator' and delete_ts is null);
+if(cnt = 0) then
+    INSERT INTO DF_NUMERATOR (ID, CREATE_TS, CREATED_BY, VERSION, CODE, NUMERATOR_FORMAT, SCRIPT_ENABLED,
+    PERIODICITY, NUMBER_INITIAL_VALUE, LOC_NAME)
+    VALUES ('6004cb0d-6baa-4eeb-8944-d89016b27f54', now(), 'system', 1, 'CarNumerator', 'CAR-[number]', FALSE, 'Y', 1,
+    '{"captionWithLanguageList":[{"language":"ru","caption":"Car"},{"language":"en","caption":"Car"}]}'
+    );
+end if;
 
+return 0;
+END;
+$$
+LANGUAGE plpgsql;
+^
+
+select baseInsert()^
+drop function if exists baseInsert()^
 --Add default numerator for cars$CarPurchaseRequest
 CREATE OR REPLACE FUNCTION baseInsert()
 RETURNS integer
@@ -78,7 +77,7 @@ cnt = (select count(id) from DF_NUMERATOR where CODE = 'CarPurchaseRequestNumera
 if(cnt = 0) then
     INSERT INTO DF_NUMERATOR (ID, CREATE_TS, CREATED_BY, VERSION, CODE, NUMERATOR_FORMAT, SCRIPT_ENABLED,
     PERIODICITY, NUMBER_INITIAL_VALUE, LOC_NAME)
-    VALUES ('33b84cdc-e08a-4b44-8309-d3cdd04beeec', now(), 'system', 1, 'CarPurchaseRequestNumerator', 'CPR-[number]', FALSE, 'Y', 1,
+    VALUES ('31c6b63f-191e-42d6-a2c0-7a9cf99c282b', now(), 'system', 1, 'CarPurchaseRequestNumerator', 'CPR-[number]', FALSE, 'Y', 1,
     '{"captionWithLanguageList":[{"language":"ru","caption":"CarPurchaseRequest"},{"language":"en","caption":"CarPurchaseRequest"}]}'
     );
 end if;
@@ -92,7 +91,7 @@ LANGUAGE plpgsql;
 select baseInsert()^
 drop function if exists baseInsert()^
 --Insert new doc type for cars$CarPurchaseRequest
-insert into TS_CARD_TYPE (ID, CREATE_TS, CREATED_BY, NAME, DISCRIMINATOR,FIELDS_XML) values ('f727e873-4823-425d-ada4-775f12c273bc', current_timestamp, 'admin', 'cars$CarPurchaseRequest', 1100, '')^
+insert into TS_CARD_TYPE (ID, CREATE_TS, CREATED_BY, NAME, DISCRIMINATOR,FIELDS_XML) values ('cc55dac5-2be9-435a-9630-7d83e87f1487', current_timestamp, 'admin', 'cars$CarPurchaseRequest', 1100, '')^
 --Add default doc kind for cars$CarPurchaseRequest
 CREATE OR REPLACE FUNCTION baseInsert()
 RETURNS integer
@@ -100,17 +99,15 @@ AS $$
 DECLARE
 cnt integer = 0;
 BEGIN
-cnt = (select count(CATEGORY_ID) from DF_DOC_KIND where category_id = 'a6db8bb8-405e-4ffb-bfa5-72724221118a');
+cnt = (select count(CATEGORY_ID) from DF_DOC_KIND where category_id = '72389c24-03c3-41db-9d1b-8a7ff5cdcf1d');
 if(cnt = 0) then
     insert into SYS_CATEGORY (ID, NAME, ENTITY_TYPE, IS_DEFAULT, CREATE_TS, CREATED_BY, VERSION, DISCRIMINATOR)
-    values ( 'a6db8bb8-405e-4ffb-bfa5-72724221118a', 'Заявка на покупку автомобиля', 'cars$CarPurchaseRequest', false, now(), USER, 1, 1);
-
+    values ( '72389c24-03c3-41db-9d1b-8a7ff5cdcf1d', 'Заявка на покупку автомобиля', 'cars$CarPurchaseRequest', false, now(), USER, 1, 1);
     insert into DF_DOC_KIND (category_id, create_ts, created_by, version, doc_type_id, numerator_id, 
     numerator_type, category_attrs_place, tab_name, portal_publish_allowed, disable_add_process_actors, create_only_by_template)
-    values ('a6db8bb8-405e-4ffb-bfa5-72724221118a', 'now()', 'admin', 1, 'f727e873-4823-425d-ada4-775f12c273bc', '4acaed60-6f43-4faa-8601-927026b14d4d', 
+    values ('72389c24-03c3-41db-9d1b-8a7ff5cdcf1d', 'now()', 'admin', 1, 'cc55dac5-2be9-435a-9630-7d83e87f1487', '31c6b63f-191e-42d6-a2c0-7a9cf99c282b', 
     1, 1, 'Р”РѕРї. РїРѕР»СЏ', false, false, false);
 end if;return 0;
-
 END;
 $$
 LANGUAGE plpgsql;
@@ -125,18 +122,16 @@ update wf_proc set updated_by='admin', card_types = card_types || 'cars$CarPurch
 
 -- begin addSecGroupConstraintsForCarPurchaseRequest
 insert into SEC_CONSTRAINT (ID, CREATE_TS, CREATED_BY, ENTITY_NAME, JOIN_CLAUSE, WHERE_CLAUSE, GROUP_ID) values 
-('48cb84dc-ba2b-40d6-a5bc-0e1feba70520', current_timestamp, 'admin', 'cars$CarPurchaseRequest', ', ts$CardAcl acl', '{E}.id = acl.card.id and (acl.user.id = :session$userId or acl.global = true)', '8e6306e2-9e10-414a-b437-24c91ffef804')^
+('e1312527-7b0a-4885-a2d4-b250d9763327', current_timestamp, 'admin', 'cars$CarPurchaseRequest', ', ts$CardAcl acl', '{E}.id = acl.card.id and (acl.user.id = :session$userId or acl.global = true)', '8e6306e2-9e10-414a-b437-24c91ffef804')^
 insert into SEC_CONSTRAINT (ID, CREATE_TS, CREATED_BY, ENTITY_NAME, JOIN_CLAUSE, WHERE_CLAUSE, GROUP_ID) values 
-('4754e739-8619-4198-8469-c631edd501eb', current_timestamp, 'admin', 'cars$CarPurchaseRequest', ', ts$CardAcl acl', '{E}.id = acl.card.id and (acl.user.id = :session$userId or acl.department.id in (:session$departmentIds) or acl.global = true)', '8d9ba07c-9ffa-11e1-b99d-8fc5b41c7fbb')^
+('69ef7aca-333b-4eb4-bfc6-52a007d0b7e0', current_timestamp, 'admin', 'cars$CarPurchaseRequest', ', ts$CardAcl acl', '{E}.id = acl.card.id and (acl.user.id = :session$userId or acl.department.id in (:session$departmentIds) or acl.global = true)', '8d9ba07c-9ffa-11e1-b99d-8fc5b41c7fbb')^
 insert into SEC_CONSTRAINT (ID, CREATE_TS, CREATED_BY, ENTITY_NAME, JOIN_CLAUSE, WHERE_CLAUSE, GROUP_ID) values 
-('995e8206-3d7d-4c23-a499-80a955a1ba82', current_timestamp, 'admin', 'cars$CarPurchaseRequest', ', ts$CardAcl acl', '{E}.id = acl.card.id and (acl.user.id = :session$userId or acl.department.id in (:session$departmentIds) or acl.global = true)', '9fa89a54-9ffa-11e1-b13e-9f4a54bff17e')^
+('c816f966-7188-4a53-ba63-39c05c9ef915', current_timestamp, 'admin', 'cars$CarPurchaseRequest', ', ts$CardAcl acl', '{E}.id = acl.card.id and (acl.user.id = :session$userId or acl.department.id in (:session$departmentIds) or acl.global = true)', '9fa89a54-9ffa-11e1-b13e-9f4a54bff17e')^
 insert into SEC_CONSTRAINT (ID, CREATE_TS, CREATED_BY, ENTITY_NAME, JOIN_CLAUSE, WHERE_CLAUSE, GROUP_ID) values 
-('74476800-3342-472b-ba0c-bba400124bf1', current_timestamp, 'admin', 'cars$CarPurchaseRequest', ', ts$CardAcl acl', '{E}.id = acl.card.id and (acl.user.id = :session$userId or acl.global = true)', '9e44a053-a31f-4edd-b19b-39e942161dd2')^
+('19c8c6cc-f898-4227-ac54-99859e521d65', current_timestamp, 'admin', 'cars$CarPurchaseRequest', ', ts$CardAcl acl', '{E}.id = acl.card.id and (acl.user.id = :session$userId or acl.global = true)', '9e44a053-a31f-4edd-b19b-39e942161dd2')^
 insert into SEC_CONSTRAINT (ID, CREATE_TS, CREATED_BY, ENTITY_NAME, JOIN_CLAUSE, WHERE_CLAUSE, GROUP_ID) values 
-('20c7a462-b075-4b60-b803-e467e17f2232', current_timestamp, 'admin', 'cars$CarPurchaseRequest', 'join {E}.aclList acl', '({E}.docOfficeData.officeFile.state >= 30 and {E}.template = false or (acl.user.id = :session$userId or acl.global = true))', 'cff945e4-e363-0dc0-d70d-4b5bdb2a2269')^
-
+('24844974-fc11-4b10-bb34-5cc70986c61c', current_timestamp, 'admin', 'cars$CarPurchaseRequest', 'join {E}.aclList acl', '({E}.docOfficeData.officeFile.state >= 30 and {E}.template = false or (acl.user.id = :session$userId or acl.global = true))', 'cff945e4-e363-0dc0-d70d-4b5bdb2a2269')^
 -- end addSecGroupConstraintsForCarPurchaseRequest
-
 insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'cars$CarPurchaseRequest:create',0,(select ID from SEC_ROLE where NAME = 'SimpleUser'));
 insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'cars$CarPurchaseRequest:update',0,(select ID from SEC_ROLE where NAME = 'SimpleUser'));
 insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'cars$CarPurchaseRequest:delete',0,(select ID from SEC_ROLE where NAME = 'SimpleUser'));
@@ -146,3 +141,8 @@ insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDAT
 insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'cars$CarPurchaseRequest:create',1,(select ID from SEC_ROLE where NAME = 'doc_initiator'));
 insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'cars$CarPurchaseRequest:update',1,(select ID from SEC_ROLE where NAME = 'doc_initiator'));
 insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'cars$CarPurchaseRequest:delete',1,(select ID from SEC_ROLE where NAME = 'doc_initiator'));
+
+-- begin addSecGroupConstraintsForCar
+insert into SEC_CONSTRAINT (ID, CREATE_TS, CREATED_BY, ENTITY_NAME, JOIN_CLAUSE, WHERE_CLAUSE, GROUP_ID) values 
+('f6559103-43f6-49ae-a05e-b57b3843a909', current_timestamp, 'admin', 'cars$Car', ', ts$CardAcl acl', '{E}.id = acl.card.id and (acl.user.id = :session$userId or acl.global = true)', '8e6306e2-9e10-414a-b437-24c91ffef804')^
+-- end addSecGroupConstraintsForCar
--- modules/core/db/update/postgres/20/200519-1413-2-updateCar01.sql
+++ modules/core/db/update/postgres/20/200519-1413-2-updateCar01.sql
@@ -1,3 +1,2 @@
 alter table CARS_CAR add constraint FK_CARS_CAR_CAR_MAKE_ID foreign key (CAR_MAKE_ID) references CARS_CAR_MAKE(ID);
-alter table CARS_CAR add constraint FK_CARS_CAR_CARD_ID foreign key (CARD_ID) references WF_CARD(ID);
 create index IDX_CARS_CAR_CAR_MAKE on CARS_CAR (CAR_MAKE_ID);
